e7cef2834a86d9e94b34e480f2d736fef41124a7
beilsn <beilsn@msn.com>
2020-06-21 19:17:06 +0800

    Update Aweme.js

--- Scripts/Aweme.js
+++ Scripts/Aweme.js
@@ -1,49 +1,75 @@
-if (typeof $response != "undefined") {
-  var obj = JSON.parse($response.body);
-  if (obj.data) {
-    for (var i = obj.data.length - 1; i >= 0; i--) {
-      if (obj.data[i].aweme.video) {
-        if (obj.data[i].aweme.status.reviewed != 1) {
-          obj.data[i].aweme.status.reviewed = 1;
-          obj.data[i].aweme.video_control.allow_download = true;
-        }
-        if (obj.data[i].aweme.video.download_addr) {
-          var play = obj.data[i].aweme.video.play_addr.url_list;
-          obj.data[i].aweme.video.download_addr.url_list = play;
-        }
-        if (obj.data[i].aweme.video.download_suffix_logo_addr) {
-          var download = obj.data[i].aweme.video.download_addr;
-          obj.data[i].aweme.video.download_suffix_logo_addr = download;
-        }
-      } else {
-        obj.data.aweme.splice(i, 1);
-      }
-    }
+const axios = require('axios').default
+
+function get(url, params, headers) {
+  return axios.get(url, { params, headers: { ...headers } })
+}
+
+function videoUrl(i) {
+  try {
+    return i.video.play_addr.url_list[0].replace('v1/play/', 'v1/playwm/')
+  } catch (error) {
+    return ''
   }
-  if (obj.aweme_list) {
-    for (var i = obj.aweme_list.length - 1; i >= 0; i--) {
-      if (obj.aweme_list[i].video) {
-        if (obj.aweme_list[i].is_ads != false) {
-          obj.aweme_list.splice(i, 1);
-        }
-        if (obj.aweme_list[i].status.reviewed != 1) {
-          obj.aweme_list[i].status.reviewed = 1;
-          obj.aweme_list[i].video_control.allow_download = true;
-        }
-        if (obj.aweme_list[i].video.download_addr) {
-          var play = obj.aweme_list[i].video.play_addr.url_list;
-          obj.aweme_list[i].video.download_addr.url_list = play;
-        }
-        if (obj.aweme_list[i].video.download_suffix_logo_addr) {
-          var download = obj.aweme_list[i].video.download_addr;
-          obj.aweme_list[i].video.download_suffix_logo_addr = download;
-        }
-      } else {
-        obj.aweme_list.splice(i, 1);
-      }
-    }
+}
+
+function coverUrl(i) {
+  try {
+    return i.video.origin_cover.url_list[0]
+  } catch (error) {
+    return ''
   }
-  $done({ body: JSON.stringify(obj) });
-} else {
-  $done({ url: $request.url.replace(/\/v\d\//, "/v1/") });
+}
+
+function transformPost(i) {
+  return {
+    id: i.aweme_id,
+    title: i.desc,
+    videoUrl: videoUrl(i),
+    coverUrl: coverUrl(i),
+    duration: i.video.duration,
+    praiseCount: i.statistics.digg_count,
+    commentCount: i.statistics.comment_count,
+  }
+}
+
+exports.postsByUser = async (
+  { uid, signature, count, cursor = 0 },
+  { headers }
+) => {
+  const { data } = await get(
+    'https://www.iesdouyin.com/web/api/v2/aweme/post/',
+    {
+      user_id: uid,
+      count,
+      max_cursor: cursor,
+      _signature: signature,
+    },
+    headers
+  )
+
+  return {
+    list: data.aweme_list.map((i) => ({
+      ...transformPost(i),
+      authorId: i.author.uid,
+      praiseCount: i.statistics.digg_count,
+      commentCount: i.statistics.comment_count,
+    })),
+    cursor: data.max_cursor,
+    isEnded: !data.has_more,
+  }
+}
+
+/**
+ * @param {string[]} ids
+ * @returns {Promise<*>}
+ */
+exports.postsByIds = async (ids) => {
+  const { data } = await get(
+    'https://www.iesdouyin.com/web/api/v2/aweme/iteminfo/',
+    {
+      item_ids: ids.join(','),
+    }
+  )
+
+  return data.item_list.map(transformPost)
 }
