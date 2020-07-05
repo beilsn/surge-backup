8c8fde20646c0065cae5f0e8317635e95f00f0cd
beilsn <beilsn@msn.com>
2020-07-05 21:51:22 +0800

    Update SSID.js

--- Scripts/SSID.js
+++ Scripts/SSID.js
@@ -1,16 +1,22 @@
 const SSID = $network.wifi.ssid;
-const proxywifi = ["K3-5G"];
+const proxywifi = !$persistentStore.read("lkWifiSsids")?["K3-5G"]:JSON.parse($persistentStore.read("lkWifiSsids"));
 let res = proxywifi.some(val => val === SSID);
-if (res) {
-$surge.setOutboundMode("direct");
-notify(1);
-} else {
-$surge.setOutboundMode("rule");
-notify(0);
+let lkWifiLast = !$persistentStore.read("lkWifiLast")?"abcdefghijklmnopqrstuvwxyz":$persistentStore.read("lkWifiLast");
+if (lkWifiLast!=(!!SSID ? SSID : "cellular")){
+    !$persistentStore.write((!!SSID ? SSID : "cellular"), "lkWifiLast")
+    if (res) {
+        $surge.setOutboundMode("direct");
+        notify(1);
+    } else {
+        $surge.setOutboundMode("rule");
+        notify(0);
+    }
 }
 
 function notify(mode) {
-setTimeout(function(){
-!!mode ? $notification.post("Wi-Fi assistant","switch to direct mode",`your SSID is ${SSID}`) : $notification.post("Wi-Fi assistant","switch to proxy mode", `${!!SSID ?  "your SSID is " + SSID : "you are using cellular"}`)
-}) }
+    setTimeout(function () {
+        !!mode ? $notification.post("Wi-Fi assistant", "switch to direct mode", `your SSID is ${SSID}`) : $notification.post("Wi-Fi assistant", "switch to proxy mode", `${!!SSID ? "your SSID is " + SSID : "you are using cellular"}`)
+    })
+}
+
 $done();
